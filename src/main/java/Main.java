654fd46465849161f75e09f31cbea75072b5e050
code-mds <43860962+code-mds@users.noreply.github.com>
2019-03-13 20:37:44 +0100

    Main view

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,20 @@
 import ij.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
 
-        System.out.println("MAIN");
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+
+        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
