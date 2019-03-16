b5768617ae2b39c3b7d61b3c9c1c73ef4df5ab67
code-mds <43860962+code-mds@users.noreply.github.com>
2019-03-16 11:36:02 +0100

    Metadata

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,20 @@
 import ij.*;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Main extends Application implements Initializable {
+
+    @FXML private MetadataController metadataController;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -16,5 +25,11 @@ public class Main extends Application {
         primaryStage.setTitle("FXML Welcome");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        InputStream input = getClass().getResourceAsStream("test.jpg");
+        metadataController.loadMetadata(input);
     }
 }
---
+++ src/main/java/MetadataController.java
@@ -0,0 +1,60 @@
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MetadataController implements Initializable {
+    @FXML
+    private GridPane gridPane;
+
+    public MetadataController()  {
+        System.out.println("CTOR");
+    }
+
+    public void loadMetadata(InputStream input) {
+        try {
+            gridPane.getChildren().clear();
+
+            int rowIndex = 0;
+
+            Metadata metadata = ImageMetadataReader.readMetadata(input);
+            for (Directory directory : metadata.getDirectories()) {
+                for (Tag tag : directory.getTags()) {
+                    System.out.println(tag);
+
+                    final Label labelGroup = new Label(tag.getDirectoryName());
+                    gridPane.add(labelGroup,0, rowIndex);
+
+                    final Label label = new Label(tag.getTagName());
+                    gridPane.add(label,1, rowIndex);
+
+                    final TextField textField = new TextField(tag.getDescription());
+                    gridPane.add(textField, 2, rowIndex);
+                    rowIndex++;
+                }
+            }
+
+            gridPane.requestLayout();
+        } catch (ImageProcessingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("INIT");
+    }
+}
