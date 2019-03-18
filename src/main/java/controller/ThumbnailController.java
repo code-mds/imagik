b9852d82d4a7cf722d5306dcef4c82c47af9edc5
mds <mds750@gmail.com>
2019-03-18 22:31:13 +0100

    Thumbnail Viewer

--- src/main/java/controller/ThumbnailController.java
+++ src/main/java/controller/ThumbnailController.java
@@ -2,75 +2,65 @@ package controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.TilePane;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ThumbnailController implements Initializable {
-    @FXML
-    private TilePane tilePane;
-
     private final String[] EXTENSIONS = new String[] {"jpg", "jpeg", "png", "gif"};
-    private final ObservableList<ImageView> imageList = FXCollections.observableArrayList();
+    private final int THUMB_SIZE = 50;
+
+    @FXML private ListView<File> thumbnails;
+
+    private final ObservableList<File> imageList = FXCollections.observableArrayList();
 
     public void update(File dir) {
         File[] files = dir.listFiles((f, name) -> acceptExtension(name.toLowerCase()));
-        for (File f : files) {
-
-            System.out.println(f.getAbsolutePath());
-                Image img = getScaledImage(f);
-                ImageView imageView = new ImageView(img);
-                imageList.add(imageView);
-                tilePane.getChildren().setAll(imageList);
-        }
+        imageList.setAll(files);
     }
 
-    private Image getScaledImage(File file){
-        try {
-            BufferedImage img2 = new BufferedImage(100, 100 ,BufferedImage.TYPE_INT_RGB);
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
-            BufferedImage sourceImage = ImageIO.read(file);
-            int width = sourceImage.getWidth();
-            int height = sourceImage.getHeight();
+        thumbnails.setItems(imageList);
 
-            if(width>height){
-                float extraSize=    height-100;
-                float percentHight = (extraSize/height)*100;
-                float percentWidth = width - ((width/100)*percentHight);
-                BufferedImage img = new BufferedImage((int)percentWidth, 100, BufferedImage.TYPE_INT_RGB);
-                java.awt.Image scaledImage = sourceImage.getScaledInstance((int)percentWidth, 100, java.awt.Image.SCALE_SMOOTH);
-                img.createGraphics().drawImage(scaledImage, 0, 0, null);
-                img2 = img.getSubimage((int)((percentWidth-100)/2), 0, 100, 100);
+        thumbnails.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("Selected item: " + newValue);
+        });
 
-            }else{
-                float extraSize=    width-100;
-                float percentWidth = (extraSize/width)*100;
-                float  percentHight = height - ((height/100)*percentWidth);
-                BufferedImage img = new BufferedImage(100, (int)percentHight, BufferedImage.TYPE_INT_RGB);
-                java.awt.Image scaledImage = sourceImage.getScaledInstance(100,(int)percentHight, java.awt.Image.SCALE_SMOOTH);
-                img.createGraphics().drawImage(scaledImage, 0, 0, null);
-                img2 = img.getSubimage(0, (int)((percentHight-100)/2), 100, 100);
+        thumbnails.setCellFactory(param -> new ListCell<File>() {
+            private ImageView imageView = new ImageView();
+
+            @Override
+            public void updateItem(File file, boolean empty) {
+                super.updateItem(file, empty);
+                if (empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    Image image = getThumbnail(file);
+                    imageView.fitHeightProperty().setValue(THUMB_SIZE);
+                    imageView.fitWidthProperty().setValue(THUMB_SIZE);
+                    imageView.setImage(image);
+
+                    setText(file.getName());
+                    setGraphic(imageView);
+                }
             }
-            Image image = SwingFXUtils.toFXImage(img2, null);
-            return image;
-
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return  null;
+        });
     }
 
+    private Image getThumbnail(File file) {
+        String uri = file.toURI().toString();
+        System.out.println(uri);
+        return new Image(uri);
+    }
 
     private boolean acceptExtension(String name) {
         for (String ext : EXTENSIONS) {
@@ -80,8 +70,38 @@ public class ThumbnailController implements Initializable {
         return false;
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        //tilePane.setItems(imageMetadataList);
-    }
+//    private Image getScaledImage(File file){
+//        try {
+//            BufferedImage img2;
+//
+//            BufferedImage sourceImage = ImageIO.read(file);
+//            int width = sourceImage.getWidth();
+//            int height = sourceImage.getHeight();
+//
+//            if(width>height){
+//                float extraSize = height-100;
+//                float percentHight = (extraSize/height)*100;
+//                float percentWidth = width - ((width/100)*percentHight);
+//                BufferedImage img = new BufferedImage((int)percentWidth, 100, BufferedImage.TYPE_INT_RGB);
+//                java.awt.Image scaledImage = sourceImage.getScaledInstance((int)percentWidth, 100, java.awt.Image.SCALE_SMOOTH);
+//                img.createGraphics().drawImage(scaledImage, 0, 0, null);
+//                img2 = img.getSubimage((int)((percentWidth-100)/2), 0, 100, 100);
+//
+//            }else{
+//                float extraSize=    width-100;
+//                float percentWidth = (extraSize/width)*100;
+//                float  percentHight = height - ((height/100)*percentWidth);
+//                BufferedImage img = new BufferedImage(100, (int)percentHight, BufferedImage.TYPE_INT_RGB);
+//                java.awt.Image scaledImage = sourceImage.getScaledInstance(100,(int)percentHight, java.awt.Image.SCALE_SMOOTH);
+//                img.createGraphics().drawImage(scaledImage, 0, 0, null);
+//                img2 = img.getSubimage(0, (int)((percentHight-100)/2), 100, 100);
+//            }
+//            Image image = SwingFXUtils.toFXImage(img2, null);
+//            return image;
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return  null;
+//    }
 }
