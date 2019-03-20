566a830e4ced967037a92061e7a4a3b244f1d313
Gatzu78 <attilio.baldini@student.supsi.ch>
2019-03-20 14:55:06 +0100

    Aggiunta icona all'app.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -13,8 +14,8 @@ public class Main extends Application  {
 
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("view/Main.fxml"));
-
         primaryStage.setTitle("Imagik Image Viewer");
+        primaryStage.getIcons().add(new Image(this.getClass().getResource("icon/wand.png").toString()));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
