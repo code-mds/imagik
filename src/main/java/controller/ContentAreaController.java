9a1afe2f18f2a252342ba5a4a29d9e8beb099850
Gatzu78 <attilio.baldini@student.supsi.ch>
2019-03-24 12:32:48 +0100

    Implementato BlackAndWhite e Reset

--- src/main/java/controller/ContentAreaController.java
+++ src/main/java/controller/ContentAreaController.java
@@ -1,10 +1,11 @@
 package controller;
 
 import com.google.common.eventbus.Subscribe;
-import event.EventManager;
-import event.EventSubscriber;
-import event.ZoomInEvent;
-import event.ZoomOutEvent;
+import event.*;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.process.ImageConverter;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,13 +13,18 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import model.MainModel;
 
+
+import javax.imageio.ImageIO;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.awt.image.BufferedImage;
 
 public class ContentAreaController implements Initializable, EventSubscriber {
     private static final float ZOOM_FACTOR = 0.1f;
-
+    private BufferedImage currentImage;
+    private File currentFile;
     @FXML ImageView imageView;
 
     @Override
@@ -32,8 +38,16 @@ public class ContentAreaController implements Initializable, EventSubscriber {
     }
 
     private void loadImage(File file) {
-        String uri = file.toURI().toString();
-        imageView.imageProperty().setValue(new Image(uri));
+        try {
+            currentFile = file;
+            currentImage = ImageIO.read(currentFile);
+            Image image = SwingFXUtils.toFXImage(currentImage, null);
+            imageView.imageProperty().setValue(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     public void zoomIn(ActionEvent e) {
@@ -67,12 +81,29 @@ public class ContentAreaController implements Initializable, EventSubscriber {
     public void resize(ActionEvent e) {
     }
 
-    public void blackWhite(ActionEvent e) {
+    public void blackWhite(ActionEvent e) { blackWhite(new BlackWhiteEvent()); }
+
+    @Subscribe
+    public void blackWhite(BlackWhiteEvent e) {
+        System.out.println("BlackAndWhite");
+        ImagePlus imageToEdit = new ImagePlus("editing image",currentImage);
+        ImageConverter imageToConvert = new ImageConverter(imageToEdit);
+        imageToConvert.convertToGray32();
+        currentImage=imageToEdit.getBufferedImage();
+        imageView.setImage(SwingFXUtils.toFXImage(currentImage,null));
+
     }
 
     public void saveChanges(ActionEvent e) {
     }
 
     public void resetChanges(ActionEvent e) {
+        resetChanges(new ResetChangesEvent());
+
+    }
+    @Subscribe
+    private void resetChanges(ResetChangesEvent resetChangesEvent) {
+        // TO DO private o public ?
+        loadImage(currentFile);
     }
 }
