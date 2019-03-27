324b8c7e4914cc520c79a5f7d80a33278d8d3aa4
Gatzu78 <attilio.baldini@student.supsi.ch>
2019-03-27 19:22:09 +0100

    Implementato Rotate Left and Right

--- src/main/java/model/ImageService.java
+++ src/main/java/model/ImageService.java
@@ -1,16 +1,29 @@
+
 package model;
 
 import ij.ImagePlus;
 import ij.process.ImageConverter;
+
 import javafx.scene.image.Image;
 
+
+
 import java.awt.image.BufferedImage;
+
 import java.io.File;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+
 
 public class ImageService {
+
     //TODO handle invalidation of edited images
+
     private final Map<File, Image> thumbnailCache = new HashMap<>();
 
     public Image getThumbnail(File file, int thumbSize) {
@@ -24,21 +37,25 @@ public class ImageService {
         }
         return image;
     }
-
     public BufferedImage rotateLeft(BufferedImage currentImage) {
         ImagePlus imageToEdit = new ImagePlus("editing image", currentImage);
         return imageToEdit.getProcessor().rotateLeft().getBufferedImage();
     }
-
     public BufferedImage rotateRight(BufferedImage currentImage){
         ImagePlus imageToEdit = new ImagePlus("editing image",currentImage);
         return imageToEdit.getProcessor().rotateRight().getBufferedImage();
     }
-
     public BufferedImage greyScale(BufferedImage currentImage) {
         ImagePlus imageToEdit = new ImagePlus("editing image",currentImage);
         ImageConverter imageToConvert = new ImageConverter(imageToEdit);
         imageToConvert.convertToGray32();
         return imageToEdit.getBufferedImage();
+    }
+    public List<BufferedImage> serviceParser(List<BufferedImage> passedImages, Function<BufferedImage,BufferedImage> serviceSelected){
+        List<BufferedImage> currentImages = new ArrayList<BufferedImage>();
+        for(int i=0;i<passedImages.size();i++){
+            currentImages.add(serviceSelected.apply(passedImages.get(i)));
+        }
+        return currentImages;
     }
 }
