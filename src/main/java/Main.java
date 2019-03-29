8c0c645f423e49a33b56aa1fd4d3ff89cb0ae04a
code-mds <43860962+code-mds@users.noreply.github.com>
2019-03-29 14:22:35 +0100

    Update maven packaging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,5 @@
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconName;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-
-import java.util.Locale;
-import java.util.ResourceBundle;
-
-
-public class Main extends Application  {
-
+public class Main   {
     public static void main(String[] args) {
-        launch(args);
+        MainApp.main(args);
     }
-
-    public void start(Stage primaryStage) throws Exception {
-        Locale locale = Locale.getDefault();
-        ResourceBundle bundle = ResourceBundle.getBundle("bundles/imagik", locale);
-        Parent root = FXMLLoader.load(getClass().getResource("view/Main.fxml"), bundle);
-        primaryStage.setTitle("Imagik Image Viewer");
-        primaryStage.getIcons().add(new Image(this.getClass().getResource("icon/wand.png").toString()));
-
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("css/style.css");
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-        //FontAwesomeIconName.
-    }
-
 }
---
+++ src/main/java/MainApp.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MainApp extends Application {
+    public static void main(String[] args) {
+        MainApp.launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        Locale locale = Locale.getDefault();
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles/imagik", locale);
+        Parent root = FXMLLoader.load(getClass().getResource("view/Main.fxml"), bundle);
+        primaryStage.setTitle("Imagik Image Viewer");
+        primaryStage.getIcons().add(new Image(this.getClass().getResource("icon/wand.png").toString()));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("css/style.css");
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        //FontAwesomeIconName.
+    }
+}
