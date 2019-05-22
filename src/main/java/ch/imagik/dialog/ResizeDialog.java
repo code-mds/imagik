7104de2751557e217d8eff9b45a84b9cdeae769c
Gatzu78 <attilio.baldini@student.supsi.ch>
2019-05-22 21:15:13 +0200

    Ristrutturata nuovamente per rispettare gli spacing e i raggruppamenti Gnome.

--- src/main/java/ch/imagik/dialog/ResizeDialog.java
+++ src/main/java/ch/imagik/dialog/ResizeDialog.java
@@ -2,15 +2,14 @@ package ch.imagik.dialog;
 
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import ch.imagik.model.MainModel;
 import ch.imagik.model.ResizeInfo;
+import javafx.scene.layout.VBox;
 
-import java.sql.SQLOutput;
 import java.util.Optional;
 
 public final class ResizeDialog {
@@ -22,41 +21,50 @@ public final class ResizeDialog {
     }
     public static Optional<ResizeInfo> show(int originalWidth, int originalHeight, boolean isMultiple) {
         double aspectRatio = (double) originalWidth / originalHeight;
-        //System.out.println("Aspect Ratio dell'immagne: "+aspectRatio);
         Dialog<ResizeInfo> dialog = new Dialog<>();
         String label;
-        label = MainModel.getInstance().getLocalizedString("resize_dialog.title");
+        if(isMultiple) {
+            label = MainModel.getInstance().getLocalizedString("resize_dialog.title_m");
+        }else{
+            label = MainModel.getInstance().getLocalizedString("resize_dialog.title_s");
+        }
         dialog.setTitle(label);
-
-        label = MainModel.getInstance().getLocalizedString("resize_dialog.header");
-        dialog.setHeaderText(label);
-
         // Set the button types.
         dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
 
-        GridPane grid = new GridPane();
-        grid.setHgap(18);
-        grid.setVgap(12);
-        System.out.println("Numero colonne: " + grid.getColumnCount());
+        GridPane gridTop = new GridPane();
+        gridTop.setHgap(12);
+        gridTop.setVgap(6);
+        gridTop.getColumnConstraints().add(0,new ColumnConstraints(60.0,60.0,60.0));
+        gridTop.setPadding(new Insets(0,0,0,0));
+        GridPane gridDown = new GridPane();
+        gridDown.setHgap(12);
+        gridDown.setVgap(6);
+        gridDown.getColumnConstraints().add(0,new ColumnConstraints(60.0,60.0,60.0));
+        gridDown.setPadding(new Insets(0,0,0,0));
         label = MainModel.getInstance().getLocalizedString("resize_dialog.percentage");
         RadioButton percentageSelector = new RadioButton(label);
+        percentageSelector.setStyle("-fx-font-weight: bold");
+        percentageSelector.getStyleClass().add("bold-label");
         percentageSelector.selectedProperty().setValue(true);
         if (isMultiple) {
-            grid.add((new Label(label)), 0, 0);
+            gridTop.add((new Label(label)), 0, 0);
         }
         SpinnerValueFactory<Integer> percentageFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 200, 100);
         Spinner<Integer> percentageValue = new Spinner<Integer>(percentageFactory);
         spinnerManipulator(percentageValue, percentageFactory);
         label = MainModel.getInstance().getLocalizedString("resize_dialog.pixel");
         RadioButton pixelSelector = new RadioButton(label);
+        pixelSelector.setStyle("-fx-font-weight: bold");
+        //pixelSelector.getStyleClass().add("bold-label");
         SpinnerValueFactory<Integer> widthFieldFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, originalWidth * 3, originalWidth);
         Spinner<Integer> widthField = new Spinner<>(widthFieldFactory);
         spinnerManipulator(widthField, widthFieldFactory);
         SpinnerValueFactory<Integer> heightFieldFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, originalHeight * 3, originalHeight);
         Spinner<Integer> heightField = new Spinner<>(heightFieldFactory);
         spinnerManipulator(heightField, heightFieldFactory);
-        grid.add(new Label("1 - 200"), 0, 1);
-        grid.add(percentageValue, 1, 1);
+        gridTop.add(new Label("1 - 200"), 0, 1);
+        gridTop.add(percentageValue, 1, 1);
         CheckBox keepRatio = new CheckBox();
         if (!isMultiple) {
             pixelSelector.selectedProperty().setValue(false);
@@ -68,26 +76,36 @@ public final class ResizeDialog {
             widthField.disableProperty().bind(percentageSelector.selectedProperty());
             keepRatio.disableProperty().bind(percentageSelector.selectedProperty());
             percentageValue.disableProperty().bind(pixelSelector.selectedProperty());
-            grid.add(percentageSelector, 1, 0);
-            grid.add(pixelSelector, 1, 2);
+            gridTop.add(percentageSelector, 1, 0);
+            gridDown.add(pixelSelector, 1, 0);
             label = MainModel.getInstance().getLocalizedString("resize_dialog.keepRatio");
-            grid.add(new Label(label), 0, 3);
-            grid.add(keepRatio, 1, 3);
+            gridDown.add(new Label(label), 0, 1);
+            gridDown.add(keepRatio, 1, 1);
             label = MainModel.getInstance().getLocalizedString("resize_dialog.width");
-            grid.add(new Label(label), 0, 4);
-            grid.add(widthField, 1, 4);
+            gridDown.add(new Label(label), 0, 2);
+            gridDown.add(widthField, 1, 2);
             label = MainModel.getInstance().getLocalizedString("resize_dialog.height");
-            grid.add(new Label(label), 0, 5);
-            grid.add(heightField, 1, 5);
+            gridDown.add(new Label(label), 0, 3);
+            gridDown.add(heightField, 1, 3);
         }
-        for (Node n : grid.getChildren()) {
+        for (Node n : gridTop.getChildren()) {
             Integer col = GridPane.getColumnIndex(n);
             int colNumber = col == null ? 0 : col.intValue();
             if (colNumber == 0) {
                 GridPane.setHalignment(n, HPos.RIGHT);
             }
         }
-
+        for (Node n : gridDown.getChildren()) {
+            Integer col = GridPane.getColumnIndex(n);
+            int colNumber = col == null ? 0 : col.intValue();
+            if (colNumber == 0) {
+                GridPane.setHalignment(n, HPos.RIGHT);
+            }
+        }
+        VBox masterContainer = new VBox();
+        masterContainer.setPadding(new Insets(18,18,18,18));
+        masterContainer.setSpacing(18.0);
+        masterContainer.getChildren().addAll(gridTop,gridDown);
         heightField.valueProperty().addListener((obs, old, newVal) -> {
             if (keepRatio.isSelected()) {
                 widthField.getValueFactory().setValue((int) Math.round(newVal * aspectRatio));
@@ -100,7 +118,8 @@ public final class ResizeDialog {
             }
         });
 
-        dialog.getDialogPane().setContent(grid);
+        dialog.getDialogPane().setContent(masterContainer);
+
 
         dialog.setResultConverter(dialogButton -> {
             if (dialogButton == ButtonType.OK) {
